17bbf171da906d5ea8dee0aaed1910ae1a8c412c
Your Name <you@example.com>
2024-08-09 20:32:39 -0700

    update

--- Components/MainComponent.tsx
+++ Components/MainComponent.tsx
@@ -1,18 +1,24 @@
+'use client'
 import React from 'react'
 import Image from 'next/image'
 import background from '../src/assets/background.svg'
-import backgroundsmall from '../src/assets/backgroundsmall.svg'
+import backgroundsmall from '../src/assets/bgsmall.svg'
 import logo from '../src/assets/logo.svg'
 import link from '../src/assets/link.svg'
 import ig from '../src/assets/ig.svg'
 import face from '../src/assets/face.svg'
 import x from '../src/assets/x.svg'
-import slash from '../src/assets/slash.svg'
-import reach from '../src/assets/reach.svg'
+import close from '../src/assets/close.svg'
 import dropdown from '../src/assets/dropdown.svg'
 import Link from 'next/link'
+import { useState } from 'react';
 
 const MainComponent = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
     return (
         <div>
             <main className="main-component main-wrap relative w-full h-[750px]">
@@ -45,9 +51,52 @@ const MainComponent = () => {
                         </ul>
                     </div>
                     <section className='dropdown items-center flex'>
-                        <Image src={dropdown} alt='' />
+                        <Image src={dropdown} alt='' onClick={toggleMenu}  />
                     </section>
                 </nav>
+                <div
+                    style={{
+                        position: 'fixed',
+                        top: 0,
+                        left: 0,
+                        width: '100%',
+                        height: '100%',
+                        backgroundColor: '#053D60',
+                        display: menuOpen ? 'flex' : 'none',
+                        flexDirection: 'column',
+                        color: 'white',
+                        fontSize: '25px',
+                        gap: '25px',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        zIndex: 1000,
+                        transition: 'all 0.3s ease',
+                    }}>
+                    <ul
+                        style={{
+                            listStyle: 'none',
+                            padding: 0,
+                            margin: 0,
+                            textAlign: 'center',
+                        }}
+                    >
+                        <div className= 'flex end absolute top-[5%] right-[8%] h-[30px]'>
+                            <Image src={close} onClick={toggleMenu}  alt =''/>
+                        </div>
+                        <li onClick={toggleMenu} style={{ margin: '20px 0' }}>
+                            <Link href="/About">About</Link>
+                        </li>
+                        <li onClick={toggleMenu} style={{ margin: '20px 0' }}>
+                            <Link href="#services">Services</Link>
+                        </li>
+                        <li onClick={toggleMenu} style={{ margin: '20px 0' }}>
+                            <Link href="#company">Company</Link>
+                        </li>
+                        <li onClick={toggleMenu} style={{ margin: '20px 0' }}>
+                            <Link href="#contact">Contact</Link>
+                        </li>
+                    </ul>
+                </div>
                 <section className="main-wrappertext flex absolute top-[200px] pl-[90px] pr-[90px] ">
                     <div className="socials gap-[40px] flex flex-col">
                         <Image src={link} alt='' />
